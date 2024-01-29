4042f612351e610e507770e678099ae3c4ab7106
mkirl <michael.e.kirl@gmail.com>
2024-01-28 19:37:38 -0600

    moved to home

---
+++ home.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, inputs, username,
+  gitUsername, gitEmail,
+  flakeDir, ... }:
+
+{
+  # Home Manager Settings
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "23.11";
+
+  # Set The Colorscheme
+  # colorScheme = inputs.nix-colors.colorSchemes."${theme}";
+
+  # Import Program Configurations
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default
+    ./config/home
+  ];
+
+  # Define Settings For Xresources
+  xresources.properties = {
+    "Xcursor.size" = 24;
+  };
+
+  # Install & Configure Git
+  programs.git = {
+    enable = true;
+    userName = "${gitUsername}";
+    userEmail = "${gitEmail}";
+  };
+
+  # Create XDG Dirs
+  xdg = {
+    userDirs = {
+        enable = true;
+        createDirectories = true;
+    };
+  };
+
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
