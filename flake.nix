8e5d2195f515f1c4932b0c23cff1c289bf944890
mkirl <michael.e.kirl@gmail.com>
2024-01-30 13:18:05 -0600

    progress parameterizing

--- flake.nix
+++ flake.nix
@@ -3,16 +3,38 @@
   description = "KirlOS";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
-  outputs = { self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     let
-      lib = nixpkgs.lib; 
+      system = "x86_64-linux";
+
+      # User Variables
+      hostname = "nixos";
+      username = "mike";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
     in {
     nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configurations.nix ];
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
      };
+    };
+    homeConfigurations = {
+      ${username} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
     };
   };
 }
