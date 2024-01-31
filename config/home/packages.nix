ed18daa40a3adadb4061ccbcbe1a6ec4f279a1a0
mkirl <michael.e.kirl@gmail.com>
2024-01-30 20:24:01 -0600

    wt

---
+++ config/home/packages.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, browser, wallpaperDir, flakeDir,
+  username, wallpaperGit, ... }:
+
+{
+  # Install Packages For The User
+  home.packages = with pkgs; [
+    pkgs."${browser}" discord libvirt swww grim slurp gnome.file-roller
+    swaynotificationcenter rofi-wayland imv transmission-gtk mpv
+    gimp obs-studio blender-hip kdenlive godot_4 rustup audacity
+    font-awesome spotify swayidle vim neovide neovim pavucontrol
+    element-desktop swaylock
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    # Import Scripts
+    (import ./../scripts/emopicker9000.nix { inherit pkgs; })
+    (import ./../scripts/task-waybar.nix { inherit pkgs; })
+    (import ./../scripts/squirtle.nix { inherit pkgs; })
+    (import ./../scripts/wallsetter.nix { inherit pkgs; inherit wallpaperDir;
+      inherit username; inherit wallpaperGit; })
+    (import ./../scripts/themechange.nix { inherit pkgs; inherit flakeDir; })
+    (import ./../scripts/theme-selector.nix { inherit pkgs; })
+    (import ./../scripts/nvidia-offload.nix { inherit pkgs; })
+  ];
+}
