06d87de3055ca52b269748b993539ae8a9b64401
mkirl <michael.e.kirl@gmail.com>
2024-01-31 10:14:44 -0600

    add sway

--- config/home/hyprland.nix
+++ config/home/hyprland.nix
@@ -69,6 +69,7 @@ in with lib; {
       misc {
         mouse_move_enables_dpms = true
         key_press_enables_dpms = false
+        disable_hyprland_logo = true
       }
       animations {
         enabled = yes
