5ce1e6bc16b768637c3feed000d672581d0fc778
mkirl <michael.e.kirl@gmail.com>
2024-02-02 16:14:35 -0600

    add plugins

--- config/home/nixvim.nix
+++ config/home/nixvim.nix
@@ -2,6 +2,20 @@
 
 {
   programs.nixvim = {
+
+    options = {
+      relativenumber = true;
+      incsearch = true;
+    };
+
+    plugins = {
+      telescope.enable = true;
+      harpoon = {
+        enable = true;
+	keymaps.addFile = "<leader>a";
+      };
+    };
+
     extraPlugins = [ pkgs.vimPlugins.catppuccin-nvim ];
     colorscheme = "catppuccin";
   };
