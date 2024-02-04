82aa1809ace1bf45c09c9af7f972743776c9639f
michael.e.kirl@gmail.com <michael.e.kirl@gmail.com>
2024-02-03 21:51:47 -0600

    anytype

--- config/home/packages.nix
+++ config/home/packages.nix
@@ -1,22 +1,52 @@
-{ pkgs, config, browser, wallpaperDir, flakeDir,
-  username, wallpaperGit, ... }:
+{ pkgs
+, config
+, browser
+, wallpaperDir
+, flakeDir
+, username
+, wallpaperGit
+, ...
+}:
 
 {
   # Install Packages For The User
   home.packages = with pkgs; [
-    pkgs."${browser}" discord libvirt swww grim slurp gnome.file-roller
-    swaynotificationcenter rofi-wayland imv transmission-gtk mpv
-    gimp obs-studio blender-hip kdenlive rustup yarn  
-    font-awesome swayidle vim pavucontrol
-    element-desktop swaylock go
+    pkgs."${browser}"
+    discord
+    libvirt
+    swww
+    grim
+    slurp
+    gnome.file-roller
+    swaynotificationcenter
+    rofi-wayland
+    imv
+    transmission-gtk
+    mpv
+    gimp
+    obs-studio
+    blender-hip
+    kdenlive
+    rustup
+    yarn
+    font-awesome
+    swayidle
+    vim
+    pavucontrol
+    element-desktop
+    swaylock
+    go
     audacity
+    anytype
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     # Import Scripts
     (import ./../scripts/emopicker9000.nix { inherit pkgs; })
     (import ./../scripts/task-waybar.nix { inherit pkgs; })
     (import ./../scripts/squirtle.nix { inherit pkgs; })
-    (import ./../scripts/wallsetter.nix { inherit pkgs; inherit wallpaperDir;
-      inherit username; inherit wallpaperGit; })
+    (import ./../scripts/wallsetter.nix {
+      inherit pkgs; inherit wallpaperDir;
+      inherit username; inherit wallpaperGit;
+    })
     (import ./../scripts/themechange.nix { inherit pkgs; inherit flakeDir; })
     (import ./../scripts/theme-selector.nix { inherit pkgs; })
     (import ./../scripts/nvidia-offload.nix { inherit pkgs; })
