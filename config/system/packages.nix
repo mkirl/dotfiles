c0a2a1a4adaad0269a6a43c34d055f60c11b07a9
michael.e.kirl@gmail.com <michael.e.kirl@gmail.com>
2024-02-18 19:55:34 -0600

    remove default hyprland

--- config/system/packages.nix
+++ config/system/packages.nix
@@ -6,13 +6,47 @@
 
   # List System Programs
   environment.systemPackages = with pkgs; [
-    wget curl git cmatrix lolcat neofetch htop btop libvirt
-    polkit_gnome lm_sensors unzip unrar libnotify eza
-    v4l-utils ydotool wl-clipboard socat cowsay lsd lshw
-    pkg-config meson hugo gnumake ninja go nodejs symbola
-    noto-fonts-color-emoji material-icons brightnessctl
-    toybox virt-viewer swappy ripgrep appimage-run 
-    networkmanagerapplet yad
+    wget
+    curl
+    git
+    cmatrix
+    lolcat
+    neofetch
+    htop
+    btop
+    libvirt
+    polkit_gnome
+    lm_sensors
+    unzip
+    unrar
+    libnotify
+    eza
+    v4l-utils
+    ydotool
+    wl-clipboard
+    socat
+    cowsay
+    lsd
+    lshw
+    pkg-config
+    meson
+    hugo
+    gnumake
+    ninja
+    go
+    nodejs
+    symbola
+    noto-fonts-color-emoji
+    material-icons
+    brightnessctl
+    toybox
+    virt-viewer
+    swappy
+    ripgrep
+    appimage-run
+    networkmanagerapplet
+    yad
+    playerctl
   ];
 
   programs.steam.gamescopeSession.enable = true;
@@ -22,7 +56,7 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     xwayland.enable = true;
   };
-  
+
   programs.fuse.userAllowOther = true;
   programs.mtr.enable = true;
   programs.gnupg.agent = {
