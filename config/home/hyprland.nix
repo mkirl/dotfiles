edfb992e356a47ab25a6fbe015472f957a93ab49
michael.e.kirl@gmail.com <michael.e.kirl@gmail.com>
2024-02-28 15:31:18 -0600

    scaling

--- config/home/hyprland.nix
+++ config/home/hyprland.nix
@@ -20,7 +20,7 @@ in with lib; {
     extraConfig = let
       modifier = "SUPER";
     in concatStrings [ ''
-      monitor=,preferred,auto,1.25
+      monitor=,preferred,auto,1.333333
       windowrule = float, ^(steam)$
       windowrule = size 1080 900, ^(steam)$
       windowrule = center, ^(steam)$
