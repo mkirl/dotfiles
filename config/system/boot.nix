9c012485461f586445cc5d0c78053a633350eb4d
michael.e.kirl@gmail.com <michael.e.kirl@gmail.com>
2024-03-07 08:16:36 -0600

    removed boot.nix

--- config/system/boot.nix
+++ config/system/boot.nix
@@ -2,7 +2,7 @@
 
 {
   # Bootloader
-  boot.loader.systemd-boot.enable = false;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernel.sysctl = { "vm.max_map_count" = 2147483642; };
   boot.tmp.useTmpfs = true;
