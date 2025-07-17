c35ef28a2fa8457d5bc5afc700a26c470791ba32
mkirl <michael.e.kirl@gmail.com>
2025-07-16 21:50:22 -0500

    remove sketchbar

--- sketchybar/.config/sketchybar/plugins/aerospace_listener.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script listens for aerospace workspace changes and updates sketchybar
-aerospace listen workspace-change exec -- sketchybar --trigger aerospace_workspace_change &
-
-# Also listen for window focus changes to update workspace indicators
-aerospace listen window-focus-change exec -- sketchybar --trigger aerospace_window_change &
